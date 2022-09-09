20c29c8ac75a7e6bd3bc970870f4ab500b82d46a
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-09-09 17:44:54 +0200

    clase camioneta

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ def __str__(self):
 
 class Coche(Vehiculo):
     def __init__(self, color, ruedas, velocidad, cilindrada): Vehiculo.__init__(self, color, ruedas)
-    self.velocidad = velocidad
-    self.cilindrada = cilindrada
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
 
     def __str__(self):
-        return Vehiculo.__str__(self) + ", {} km/h, {}
-cc".format(self.velocidad, self.cilindrada)
+        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
 
 
-c = Coche("azul", 4, 150, 1200)
-print(c)
+class Camioneta(Vehiculo):
+    def __init__(self, color, ruedas, carga): Vehiculo.__init__(self, color, ruedas)
+        self.carga = carga
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.carga)
+
+
+coche1 = Coche("azul", 4, 150, 1200)
+
+print(coche)
