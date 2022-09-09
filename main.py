111e71845ef5e9ff0778da58f56c8d89910e8055
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-09-09 17:50:43 +0200

    basico

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing_extensions import Self
+
+
 class Vehiculo():
     def __init__(self, color, ruedas):
         self.color = color
@@ -7,7 +10,7 @@ def __str__(self):
 
 class Coche(Vehiculo):
     def __init__(self, color, ruedas, velocidad, cilindrada): Vehiculo.__init__(self, color, ruedas)
-        self.velocidad = velocidad
+        Self.velocidad = velocidad
         self.cilindrada = cilindrada
 
     def __str__(self):
@@ -19,7 +22,24 @@ class Camioneta(Vehiculo):
         self.carga = carga
 
     def __str__(self):
-        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.carga)
+        return Vehiculo.__str__(self) + ", {} kg".format(self.carga)
+
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo): Vehiculo.__init__(self, color, ruedas)
+        self.tipo = tipo
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {}".format(self.tipo)
+
+
+class Motocicleta(Vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada): Vehiculo.__init__(self, color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} km/h, {}cc".format(self.velocidad, self.cilindrada)
 
 
 coche1 = Coche("azul", 4, 150, 1200)
