7fc2e30c4cdfdf2c8130dde68e1f469f33ef7612
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-09-10 17:32:46 +0200

    codigo antes de poo

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from typing_extensions import Self
-
-from pytest import Class
-
 
 class Vehiculo():
     def __init__(self, color, ruedas):
         self.color = color
         self.ruedas = ruedas
-def __str__(self):
-    return "Color {}, {} ruedas".format( self.color, self.ruedas )
+    def __str__(self):
+        return "Color {}, {} ruedas".format( self.color, self.ruedas )
 
 class Coche(Vehiculo):
-    def __init__(self, color, ruedas, velocidad, cilindrada): Vehiculo.__init__(self, color, ruedas)
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        Vehiculo.__init__(self, color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
 
@@ -20,7 +17,8 @@ class Coche(Vehiculo):
 
 
 class Camioneta(Vehiculo):
-    def __init__(self, color, ruedas, carga): Vehiculo.__init__(self, color, ruedas)
+    def __init__(self, color, ruedas, carga):
+        Vehiculo.__init__(self, color, ruedas)
         self.carga = carga
 
     def __str__(self):
@@ -28,7 +26,8 @@ class Camioneta(Vehiculo):
 
 
 class Bicicleta(Vehiculo):
-    def __init__(self, color, ruedas, tipo): Vehiculo.__init__(self, color, ruedas)
+    def __init__(self, color, ruedas, tipo):
+        Vehiculo.__init__(self, color, ruedas)
         self.tipo = tipo
 
     def __str__(self):
@@ -36,7 +35,8 @@ class Bicicleta(Vehiculo):
 
 
 class Motocicleta(Vehiculo):
-    def __init__(self, color, ruedas, velocidad, cilindrada): Vehiculo.__init__(self, color, ruedas)
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        Vehiculo.__init__(self, color, ruedas)
         self.velocidad = velocidad
         self.cilindrada = cilindrada
 
@@ -45,5 +45,59 @@ class Motocicleta(Vehiculo):
 
 
 coche1 = Coche("azul", 4, 150, 1200)
+camioneta1=Camioneta('verde', 4, 1900)
+bicicleta1=Bicicleta('amarilla', 2, 'deportiva')
+motocicleta1=Motocicleta('marron', 2, 300, 1100)
 
-print(coche)
+print(coche1)
+
+# print(camioneta1)
+
+# print(bicicleta1)
+
+# print(motocicleta1)
+
+lista_vehiculos = [coche1, camioneta1, bicicleta1, motocicleta1]
+
+
+def catalogar(lista):
+    for vehiculo in lista:
+
+        print(type(vehiculo).__name__)
+        print(vehiculo.color)
+        print (vehiculo.ruedas)
+
+        if type(vehiculo).__name__=='Coche':
+            print(vehiculo.velocidad)
+            print(vehiculo.cilindrada)
+
+
+        if type(vehiculo).__name__=='Motocicleta':
+            print(vehiculo.velocidad)
+            print(vehiculo.cilindrada)
+
+        if type(vehiculo).__name__=='Bicicleta':
+            print(vehiculo.tipo)
+
+        if type(vehiculo).__name__=='Camioneta':
+            print(vehiculo.carga)
+
+
+catalogar(lista_vehiculos)
+
+
+
+def catalogar2(ruedas_pedidas, lista):
+
+    vehiculos_ruedas=0
+    for vehiculo in lista:
+        if vehiculo.ruedas==ruedas_pedidas:
+            vehiculos_ruedas+=1
+
+    print(vehiculos_ruedas)
+
+
+catalogar2(4, lista_vehiculos)
+
+
+
